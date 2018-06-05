6a880add8abb86d5bdd1e29b39e447e7df779818
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-06-04 20:59:51 -0500

    first push for the tics, tacs n toes

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
-});
+  var clear = '';
+  $('#clear').on('click', function(){
+    $('[data-cell]').text('');
+  })
+
+ var turn = 'X';
+ $('[data-cell]').on('click', function(){
+   if($(this).text()===''){
+    $(this).text(turn);
+    if(turn === '0'){
+      turn = 'X';
+    }else{
+      turn = '0';
+    }
+   }
+ })
+
+
+}
+
+);
