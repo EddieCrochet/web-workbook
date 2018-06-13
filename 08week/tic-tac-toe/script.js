bf48c2ac0b7b76fbaef28c106bbccd142cd19191
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-06-13 17:47:27 -0500

    not done...

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     $('[data-cell]').text('');
   })
 
+
  var turn = 'X';
  $('[data-cell]').on('click', function(){
    if($(this).text()===''){
@@ -15,10 +16,26 @@ $(document).ready(function() {
     }else{
       turn = '0';
     }
-   }
+   }     
  })
 
+var pic1 = $("[data-cell=0]").text;
+var pic2 = $("[data-cell=1]").text;
+var pic3 = $("[data-cell=2]").text;
+var pic4 = $("[data-cell=3]").text;
+var pic5 = $("[data-cell=4]").text;
+var pic6 = $("[data-cell=5]").text;
+var pic7 = $("[data-cell=6]").text;
+var pic8 = $("[data-cell=7]").text;
+var pic9 = $("[data-cell=8]").text;
 
+ function checkWin(){
+  if (pic1 === pic2 && pic3){
+    console.log('pic1');
+    $("#announce-winner").innerHTML = "Game won! Please reset the game.";
+ }
+}
+checkWin();
 }
 
 );
