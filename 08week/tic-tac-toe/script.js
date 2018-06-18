70913374a4b7b2dd17d1a932c5797ea8b909a61a
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-06-17 22:12:24 -0500

    this one was tough for me...

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,41 +1,57 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
+  let playerOne = 'X';
+  let playerTwo = '0';
+  var pic1 = $("[data-cell=0]").text;
+  var pic2 = $("[data-cell=1]").text;
+  var pic3 = $("[data-cell=2]").text;
+  var pic4 = $("[data-cell=3]").text;
+  var pic5 = $("[data-cell=4]").text;
+  var pic6 = $("[data-cell=5]").text;
+  var pic7 = $("[data-cell=6]").text;
+  var pic8 = $("[data-cell=7]").text;
+  var pic9 = $("[data-cell=8]").text;
+  var turn = 1;
   var clear = '';
-  $('#clear').on('click', function(){
+ 
+
+  $('[data-cell]').click(function () {
+
+    if (turn % 2 === 1) {
+      $(this).text(playerOne);
+      checkWin(playerOne);
+    } else {
+      $(this).text(playerTwo);
+      checkWin(playerTwo);
+    }
+    turn++
+  })
+
+  //  $('[data-cell]').on('click', function(){
+  //    if($(this).text(playerOne)){
+  //     $(this).text(turn);
+  //     checkWin(playerOne);
+  //     if(turn === playerTwo){
+  //       turn = playerOne;
+  //     }else{
+  //       turn = playerTwo;
+  //     }
+  //     }     
+  //  })
+
+  function checkWin() {
+    if (pic1 === playerOne && pic2 === playerOne && pic3 === playerOne) {
+      console.log(pic1, "is this working?");
+      $("#announce-winner").innerHTML = "the player playing with the x is the winner";
+    }
+  }
+
+  $('#clear').on('click', function () {
     $('[data-cell]').text('');
   })
 
 
- var turn = 'X';
- $('[data-cell]').on('click', function(){
-   if($(this).text()===''){
-    $(this).text(turn);
-    if(turn === '0'){
-      turn = 'X';
-    }else{
-      turn = '0';
-    }
-   }     
- })
-
-var pic1 = $("[data-cell=0]").text;
-var pic2 = $("[data-cell=1]").text;
-var pic3 = $("[data-cell=2]").text;
-var pic4 = $("[data-cell=3]").text;
-var pic5 = $("[data-cell=4]").text;
-var pic6 = $("[data-cell=5]").text;
-var pic7 = $("[data-cell=6]").text;
-var pic8 = $("[data-cell=7]").text;
-var pic9 = $("[data-cell=8]").text;
-
- function checkWin(){
-  if (pic1 === pic2 && pic3){
-    console.log('pic1');
-    $("#announce-winner").innerHTML = "Game won! Please reset the game.";
- }
-}
-checkWin();
 }
 
 );
