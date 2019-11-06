1413cd5fd1ec2c8fb774cb42a69c17afd1be0e21
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-11-05 20:46:25 -0600

    my check for win function is dead not working!

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,15 +2,15 @@
 $(document).ready(function () {
   let playerOne = 'X';
   let playerTwo = '0';
-  var pic1 = $("[data-cell=0]").text;
-  var pic2 = $("[data-cell=1]").text;
-  var pic3 = $("[data-cell=2]").text;
-  var pic4 = $("[data-cell=3]").text;
-  var pic5 = $("[data-cell=4]").text;
-  var pic6 = $("[data-cell=5]").text;
-  var pic7 = $("[data-cell=6]").text;
-  var pic8 = $("[data-cell=7]").text;
-  var pic9 = $("[data-cell=8]").text;
+  var pic0 = $("[data-cell=0]").text;
+  var pic1 = $("[data-cell=1]").text;
+  var pic2 = $("[data-cell=2]").text;
+  var pic3 = $("[data-cell=3]").text;
+  var pic4 = $("[data-cell=4]").text;
+  var pic5 = $("[data-cell=5]").text;
+  var pic6 = $("[data-cell=6]").text;
+  var pic7 = $("[data-cell=7]").text;
+  var pic8 = $("[data-cell=8]").text;
   var turn = 1;
   var clear = '';
  
@@ -19,10 +19,10 @@ $(document).ready(function () {
 
     if (turn % 2 === 1) {
       $(this).text(playerOne);
-      checkWin(playerOne);
+      checkWin();
     } else {
       $(this).text(playerTwo);
-      checkWin(playerTwo);
+      checkWin();
     }
     turn++
   })
@@ -40,17 +40,39 @@ $(document).ready(function () {
   //  })
 
   function checkWin() {
-    if (pic1 === playerOne && pic2 === playerOne && pic3 === playerOne) {
-      console.log(pic1, "is this working?");
-      $("#announce-winner").innerHTML = "the player playing with the x is the winner";
+    console.log("checking win");
+    if ((pic0 === playerOne && pic1 === playerOne && pic2 === playerOne) ||
+      (pic3 === playerOne && pic4 === playerOne && pic5 === playerOne) ||
+      (pic6 === playerOne && pic7 === playerOne && pic8 === playerOne)) {
+        console.log("is this working?");
+        $("announce-winner").innerHTML = "the player playing with the X is the winner";
+    } else if ((pic0 === playerOne && pic1 === playerOne && pic2 === playerOne) ||
+    (pic3 === playerOne && pic4 === playerOne && pic5 === playerOne) ||
+    (pic6 === playerOne && pic7 === playerOne && pic8 === playerOne)) {  
+      console.log("is this working? OR NOT!?");
+      $("announce-winner").innerHTML = "the player playing with the 0 is the winner";
     }
   }
+
+  /*function checkWin() {
+    console.log("Checking win!");
+    if ((pic0 === pic1) && (pic0 === pic2) ||
+        (pic3 === pic4) && (pic3 === pic5) ||
+        (pic6 === pic7) && (pic6 === pic8) ||
+        (pic0 === pic3) && (pic0 === pic6) ||
+        (pic1 === pic4) && (pic1 === pic7) ||
+        (pic2 === pic5) && (pic2 === pic8)) {
+          document.getElementsByClassName("announce-winner").innerHTML = "LOOKS LIKE WE GOT A WINNER!";
+          console.log('itsa looka likea wina');
+        } else {
+          document.getElementsByClassName("announce-winner").innerHTML = "NO WINNER YET HAHA!";
+          console.log('itsa looka likea NOTA wina');
+        }
+  }*/
 
   $('#clear').on('click', function () {
     $('[data-cell]').text('');
   })
-
-
 }
 
 );
@@ -74,7 +96,7 @@ $(document).ready(function() {
       console.log(counter);
     })
     if(counter === end){
-      $('#announce-winner').text('you hit ${limit}!!')
+      $('.announce-winner').text('you hit ${limit}!!')
     }
   }
 })
